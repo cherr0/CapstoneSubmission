bffa806872294bb5924bd6ae83b10d18b9ba9d26
sihyeon955 <75560041+sihyeon955@users.noreply.github.com>
2021-01-22 02:11:23 +0900

    프로그래스 바

---
+++ sihyeon/android/Progress/app/src/main/java/com/example/sampleprogress/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.sampleprogress;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    ProgressDialog dialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ProgressBar progressBar = findViewById(R.id.progressBar);//프로그래스바 객체 참조하기
+        progressBar.setIndeterminate(false);
+        progressBar.setProgress(80);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog = new ProgressDialog(MainActivity.this);
+                dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                dialog.setMessage("데이터를 확인하는 중입니다.");
+
+                dialog.show();
+            }
+        });
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onclick(View view) {
+                if(dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+    }
+}
