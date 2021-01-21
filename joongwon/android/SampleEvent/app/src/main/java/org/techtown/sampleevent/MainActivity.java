fc7f2bfbf8e494e963a0227132dede264a7f00eb
JangWonJun <dnjswns0415@gmail.com>
2021-01-22 00:04:26 +0900

    Revert "Merge branch 'main' of https://github.com/cherr0/CapstoneSubmission into main"
    This reverts commit 4ed6b55c9e99c7100f19f260dc1e20d2bc34232b, reversing
    changes made to 87ce11e3e365b0ef3fcb895b4c53fcc3e24dacc0.

--- joongwon/android/SampleEvent/app/src/main/java/org/techtown/sampleevent/MainActivity.java
+++ joongwon/android/SampleEvent/app/src/main/java/org/techtown/sampleevent/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.textView);
 
-        View view = findViewById(R.id.view);
+        View view = findViewById(R.id.textView);
         view.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
