a3d56b0cfb6654ac0cf39dba33e9f9f811b7cce7
cherr0 <adsl1664@gmail.com>
2021-03-07 17:00:13 +0900

    홍드로이드 스레드

---
+++ joongwon/android/ThreadExample/app/src/main/java/com/codesample/MainActivity.java
@@ -0,0 +1,66 @@
+package com.codesample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_start, btn_stop;
+    Thread thread;
+    boolean isThread = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // 스레드 시작
+        btn_start = (Button) findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                isThread = true;
+                thread = new Thread() {
+                  public void run() {
+                      while(isThread) {
+                          try {
+                              sleep(2000);
+                          } catch (InterruptedException e) {
+                              e.printStackTrace();
+                          }
+
+                          handler.sendEmptyMessage(0);
+                      }
+                  }
+                };
+                thread.start();
+            }
+        });
+
+        // 스레드 종료
+        btn_stop = (Button) findViewById(R.id.btn_stop);
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isThread = false;
+            }
+        });
+    }
+
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            Toast.makeText(getApplicationContext(), "안드로이드 강의", Toast.LENGTH_SHORT);
+        }
+    };
+}
